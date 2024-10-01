58e577cce0b10312aeb4df7b9b6a3dbdfcd2bce0
mcDandy <danda.martinek@gmail.com>
2024-10-01 22:43:04 +0200

    Zkopírována část tutoriálu https://www.oracle.com/webfolder/technetwork/tutorials/obe/db/dotnet/ODPNET_Core_get_started/index.html

--- BDAS_2_dog_shelter/MainWindow.xaml.cs
+++ BDAS_2_dog_shelter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
 
 namespace BDAS_2_dog_shelter
 {
@@ -16,9 +18,44 @@ namespace BDAS_2_dog_shelter
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string ConnectionString = "User Id=hr;Password=<password>;Data Source=<ip or hostname>:1521/<service name>;";
+
+
         public MainWindow()
         {
             InitializeComponent();
+            using (OracleConnection con = new OracleConnection(ConnectionString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select first_name from employees where department_id = :id";
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", 50);
+                        cmd.Parameters.Add(id);
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())//for every row
+                        {
+                             //await reader.GetString("name");
+                        }
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)//something went wrong
+                    {
+                        
+                    }
+                }
+
+            }
         }
-    }
 }
