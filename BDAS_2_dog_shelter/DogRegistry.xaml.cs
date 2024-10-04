f6990917624bdd45c5bc69132a5d89667d316c31
mcDandy <danda.martinek@gmail.com>
2024-10-05 00:01:14 +0200

    Přidáno nové okno se stránkami (Nebudou se muset vytvářet další okna)

--- BDAS_2_dog_shelter/DogRegistry.xaml.cs
+++ BDAS_2_dog_shelter/DogRegistry.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BDAS_2_dog_shelter
 {
     /// <summary>
-    /// Interakční logika pro MainWindow.xaml
+    /// Interakční logika pro DogRegistry.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DogRegistry : Page
     {
-        public MainWindow()
+        public DogRegistry()
         {
             InitializeComponent();
-            
         }
     }
 }
---
+++ BDAS_2_dog_shelter/LoginWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+
+namespace BDAS_2_dog_shelter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        const string ConnectionString = "User Id=hr;Password=<password>;Data Source=<ip or hostname>:1521/<service name>;";
+
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            using (OracleConnection con = new OracleConnection(ConnectionString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select first_name from employees where department_id = :id";
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", 50);
+                        cmd.Parameters.Add(id);
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())//for every row
+                        {
+                            //await reader.GetString("name");
+                        }
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)//something went wrong
+                    {
+
+                    }
+                }
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow mw = new LoginWindow();
+            this.Close();
+            mw.Show();
+        }
+    }
+}
