6f1c83f935ee624d6e3d88e17b13373481027477
mcDandy <danda.martinek@gmail.com>
2024-10-15 19:36:54 +0200

    přidána registrace

--- BDAS_2_dog_shelter/LoginWindow.xaml.cs
+++ BDAS_2_dog_shelter/LoginWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Oracle.ManagedDataAccess.Client;
 using static BDAS_2_dog_shelter.Secrets;
+using System.Security.Cryptography;
+using System.Text;
+
 
 namespace BDAS_2_dog_shelter
 {
@@ -19,8 +22,13 @@ namespace BDAS_2_dog_shelter
             }
             catch (System.TypeInitializationException)
             {
-                MessageBox.Show("Nenašel se soubor secrets.json s tajnými konstantami ve složce programu nebo formát souboru není JSON.","Chyba",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("Nenašel se soubor secrets.json s tajnými konstantami ve složce programu nebo formát souboru není JSON.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             using (OracleConnection con = new OracleConnection(ConnectionString))
             {
                 using (OracleCommand cmd = con.CreateCommand())
@@ -32,17 +40,21 @@ namespace BDAS_2_dog_shelter
 
                         //Use the command to display employee names from 
                         // the EMPLOYEES table
-                        cmd.CommandText = "select jmeno from psi where vek > :id";
+                        cmd.CommandText = "select count(*) from USERS where UNAME = :id and PASSWD = :pw";
+                        SHA256 sha256 = SHA256.Create();
 
                         // Assign id to the department number 50 
-                        OracleParameter id = new OracleParameter("id", 10_000_000+"; select 10 from dual as fail");
+                        OracleParameter id = new OracleParameter("id", inp_username.Text);
+                        OracleParameter id1 = new OracleParameter("pw", sha256.ComputeHash(Encoding.UTF8.GetBytes(inp_password.Text)));
                         cmd.Parameters.Add(id);
+                        cmd.Parameters.Add(id1);
 
                         //Execute the command and use DataReader to display the data
                         OracleDataReader reader = cmd.ExecuteReader();
                         while (reader.Read())//for every row
                         {
-                            /*await*/ MessageBox.Show(reader.GetString(0));
+                            /*await*/
+                            MessageBox.Show(reader.GetString(0));
                         }
 
                         reader.Dispose();
@@ -54,13 +66,43 @@ namespace BDAS_2_dog_shelter
                 }
 
             }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
             MainWindow mw = new MainWindow();
             this.Close();
             mw.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (OracleConnection con = new OracleConnection(ConnectionString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "insert into USERS (UNAME,PASSWD) VALUES (:id, :pw)";
+                        SHA256 sha256 = SHA256.Create();
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", inp_username.Text);
+                        OracleParameter id1 = new OracleParameter("pw", sha256.ComputeHash(Encoding.UTF8.GetBytes(inp_password.Text)));
+                        cmd.Parameters.Add(id);
+                        cmd.Parameters.Add(id1);
+
+                        //Execute the command and use DataReader to display the data
+                         cmd.ExecuteNonQuery();
+                        con.Commit();
+                    }
+                    catch (Exception ex)//something went wrong
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
