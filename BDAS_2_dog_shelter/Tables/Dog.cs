4207e6939008951f59d43f44c3479d804431db2d
mcDandy <danda.martinek@gmail.com>
2024-11-05 19:16:57 +0100

    pes oprava?

--- BDAS_2_dog_shelter/Tables/Dog.cs
+++ BDAS_2_dog_shelter/Tables/Dog.cs
@@ -120,7 +120,7 @@ namespace BDAS_2_dog_shelter.Tables
                 }
             }
         }
-        public Dog() { _name = ""; _age = 0; _body_color = ""; _datum_prijeti = DateTime.Now; _duvod_prijeti = ""; _stav_pes = ""; byte[] b = new byte[256 * 256 * 4]; new Random().NextBytes(b); _obrazek = BitmapSource.Create(256, 256, 96, 96, PixelFormats.Bgra32, null, b, 256*4); }
+        public Dog() { ID = sequence++; _name = ""; _age = 0; _body_color = ""; _datum_prijeti = DateTime.Now; _duvod_prijeti = ""; _stav_pes = ""; byte[] b = new byte[256 * 256 * 4]; new Random().NextBytes(b); _obrazek = BitmapSource.Create(256, 256, 96, 96, PixelFormats.Bgra32, null, b, 256*4); }
         public Dog(string name, int age, string bodycolor, DateTime datumPrijeti, string duvodPrijeti,string stavPes, int utulekid, BitmapSource OBRAZEK) 
         { 
             _name = name;
