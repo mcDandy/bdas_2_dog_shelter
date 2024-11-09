0641722ca6eb66f15296b4d1ffc5f10f2e920e21
mcDandy <danda.martinek@gmail.com>
2024-11-09 11:23:37 +0100

    Moving files around

---
+++ BDAS_2_dog_shelter/Add/Dog/AddDogModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BDAS_2_dog_shelter.Add.Dog
+{
+    [ObservableObject]
+    internal partial class AddDogViewModel
+    {
+        public AddDogViewModel() {
+        }
+    }
+}
