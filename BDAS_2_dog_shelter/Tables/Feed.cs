a0f3bd37b1699f17997e2618f36e1b1d9653b228
Miren Lup <Mirenlup@outlook.cz>
2024-12-01 15:34:01 +0100

    Vytvoření tříd pro tabulky v databazi

---
+++ BDAS_2_dog_shelter/Tables/Feed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDAS_2_dog_shelter.Tables
+{
+    internal class Feed
+    {
+    }
+}
