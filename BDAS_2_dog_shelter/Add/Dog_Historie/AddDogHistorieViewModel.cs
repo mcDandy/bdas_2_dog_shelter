182fc6a92e2c61046d980a1224708fd997ffc465
mcDandy <danda.martinek@gmail.com>
2024-12-03 19:43:40 +0100

    dirty fix 2

--- BDAS_2_dog_shelter/Add/Dog_Historie/AddDogHistorieViewModel.cs
+++ BDAS_2_dog_shelter/Add/Dog_Historie/AddDogHistorieViewModel.cs
@@ -16,7 +16,7 @@ namespace BDAS_2_dog_shelter.Add.Dog_Historie
         private int? sklad;
         RelayCommand okCommand;
 
-        public ICommand OkHCommand => okCommand ??= new RelayCommand(Ok, () => name is not null and not "" && sklad is not null and not < 0 && pocet is not < 0);
+        public ICommand OkHCommand => okCommand ??= new RelayCommand(Ok);
 
         public delegate void OkHistorieAddEditDone();
         public event OkHistorieAddEditDone? OkClickFinished;
@@ -28,7 +28,7 @@ namespace BDAS_2_dog_shelter.Add.Dog_Historie
         }
 
         public string Nazev { get => name; set { name = value; if (okCommand is not null) okCommand.NotifyCanExecuteChanged(); } }
-        public DateTime Pocet { get => pocet; set => pocet = value; }
+        public DateTime Pocet { get => pocet??DateTime.Now; set => pocet = value; }
         public int? ID { get => iD; set => iD = value; }
         public int? SkladID { get => sklad; set { sklad = value; if (okCommand is not null) okCommand.NotifyCanExecuteChanged(); } }
         public Tables.Dog_History historie => d;
