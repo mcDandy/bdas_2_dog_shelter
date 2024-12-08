55819a7523c4ad445aeaa9f9dd77235752e95c5e
Fidorka2023 <st69598@upce.cz>
2024-12-08 21:57:35 +0100

    uprava dogtreeviewmodel

--- BDAS_2_dog_shelter/DogTree/DogTreeViewModel.cs
+++ BDAS_2_dog_shelter/DogTree/DogTreeViewModel.cs
@@ -1,33 +1,52 @@
 ﻿using BDAS_2_dog_shelter.Tables;
 using System.ComponentModel;
+using System.Windows.Media.Imaging;
 
 namespace BDAS_2_dog_shelter.DogTree
 {
-    internal class DogTreeViewModel :INotifyPropertyChanged
+    internal class DogTreeViewModel : INotifyPropertyChanged
+    {
+        private Dog _dog;
+
+        public DogTreeViewModel(Dog dog)
         {
-            private Dog dog;
+            Dog = dog;
+            LoadDogTree(); // Načíst data
+        }
 
-            public DogTreeViewModel(Dog d)
+        public Dog Dog
+        {
+            get => _dog;
+            set
             {
-                dog = d;
-                LoadDogTree(); // Načíst data
-            }
-
-            public string DogName => dog?.Name ?? "No name";
-            public string FatherName => dog.Otec?.Name ?? "No father"; // Zde používáme vlastnost Otec
-            public string MotherName => dog.Matka?.Name ?? "No mother"; // Zde používáme vlastnost Matka
-
-            private void LoadDogTree()
-            {
-                // Můžete rozšiřovat funkcionalitu, například načítat další data, pokud by byla třeba
-            }
-
-            public event PropertyChangedEventHandler? PropertyChanged;
-
-            protected void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                if (_dog != value)
+                {
+                    _dog = value;
+                    OnPropertyChanged(nameof(Dog));
+                    OnPropertyChanged(nameof(DogName));
+                    OnPropertyChanged(nameof(FatherName));
+                    OnPropertyChanged(nameof(MotherName));
+                    // Další vlastnosti, pokud potřebujete
+                }
             }
         }
 
+        public string DogName => Dog?.Name ?? "No name";
+        public string FatherName => Dog?.Otec?.Name ?? "No father";
+        public string MotherName => Dog?.Matka?.Name ?? "No mother";
+
+        public BitmapSource DogImage => Dog?.Obrazek; // Přidáno pro obrázek
+
+        private void LoadDogTree()
+        {
+            // Může se implementovat další logika pro načítání dat
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+}
